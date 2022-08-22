617563e762cd1ea5a1348d6ff9fa40c03272a4e4
wangpengcheng <1787561180@qq.com>
2022-08-22 23:34:27 +0800

    1

---
+++ push.py
@@ -0,0 +1,30 @@
+# -*- coding: utf8 -*-
+# ！/usr/bin/env python
+
+import os
+import shutil
+import sys
+
+# git clone的目录
+gitDir = "C:\\Users\\1\\Desktop\\gittemp\\"
+# 文件分片的目录
+fileDir = "C:\\Users\\1\\Desktop\\gulivideo\\"
+
+def push():
+    """
+    获取目录所有分片文件
+    """
+    list = os.listdir(fileDir)
+    for name in list:
+        if ".rar" in name:
+            from_file_path = fileDir + name
+            target_file_path = gitDir + name
+            print(from_file_path)
+            print(target_file_path)
+            shutil.move(from_file_path,target_file_path)
+            os.system("git add . && git commit -m 1 && git push")
+    exit()        
+
+
+if __name__=='__main__':
+   push()
